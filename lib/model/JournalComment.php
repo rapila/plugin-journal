ef5334dc1de363dff2eb17dbca380ff0839f6896
Jürg Messmer <jm@mosaics.ch>
2012-07-05 06:07:04 +0200

    updated to new propel

--- lib/model/JournalComment.php
+++ lib/model/JournalComment.php
@@ -4,21 +4,22 @@ require_once 'model/om/BaseJournalComment.php';
 
 
 /**
- * Skeleton subclass for representing a row from the 'journal_comments' table.
- *
- * 
- *
- * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
- * long as it does not already exist in the output directory.
- *
  * @package model
  */	
 class JournalComment extends BaseJournalComment {
+	
+	public function getCreatedAtLocalized($sFormat = ' %e. %B %Y') {
+		if(Session::language() === 'en') {
+			$sFormat = ' %e %B %Y';
+		}
+		return LocaleUtil::localizeDate($this->created_at, null, $sFormat);
+	}
+	
 	public function preSave(PropelPDO $oConnection = null) {
 		if($this->isNew()) {
 			$this->setActivationHash(Util::uuid());
 		}
 		return parent::preSave($oConnection);
 	}
-} // JournalComment
+}
+
--- lib/model/JournalEntry.php
+++ lib/model/JournalEntry.php
@@ -5,42 +5,49 @@ require_once 'model/om/BaseJournalEntry.php';
 
 /**
  * @package model
- */	
+ */ 
 class JournalEntry extends BaseJournalEntry {
 
+	public function getCreatedAtLocalized($sFormat = ' %e. %B %Y') {
+		if(Session::language() === 'en') {
+			$sFormat = ' %e %B %Y';
+		}
+		return LocaleUtil::localizeDate($this->created_at, null, $sFormat);
+	}
+	
 	public function getJournalComments($oCriteria = null, PropelPDO $oConnection = null) {
-	  if($oCriteria === null) {
-      $oCriteria = new Criteria();
-	  }
-    $oCriteria->addAscendingOrderByColumn(JournalCommentPeer::CREATED_AT);
-    $oCriteria->addAscendingOrderByColumn(JournalCommentPeer::ID);
-    return parent::getJournalComments($oCriteria, $oConnection);
+		if($oCriteria === null) {
+			$oCriteria = new Criteria();
+		}
+		$oCriteria->addAscendingOrderByColumn(JournalCommentPeer::CREATED_AT);
+		$oCriteria->addAscendingOrderByColumn(JournalCommentPeer::ID);
+		return parent::getJournalComments($oCriteria, $oConnection);
 	}
 	
 	public function getRssAttributes($oJournalPage = null, $bIsForRpc = false) {
-    $aResult = array();
-    $aResult['title'] = $this->getTitle();
-    $aResult['link'] = LinkUtil::absoluteLink(LinkUtil::link($this->getLink($oJournalPage), 'FrontendManager'));
+		$aResult = array();
+		$aResult['title'] = $this->getTitle();
+		$aResult['link'] = LinkUtil::absoluteLink(LinkUtil::link($this->getLink($oJournalPage), 'FrontendManager'));
 		if($bIsForRpc) {
 			$aResult['description'] = RichtextUtil::parseStorageForBackendOutput($this->getText())->render();
 		} else {
 			$aResult['description'] = RichtextUtil::parseStorageForFrontendOutput($this->getText())->render();
 		}
-    $aResult['author'] = $this->getUserRelatedByCreatedBy()->getEmail().' ('.$this->getUserRelatedByCreatedBy()->getFullName().')';
-    $aTags = TagPeer::tagInstancesForObject($this);
-    $aCategories = array();
-    foreach($aTags as $oTag) {
-      $aCategories[] = $oTag->getTagName();
-    }
-    $aResult[$bIsForRpc ? 'categories' : 'category'] = $aCategories;
-    $aResult['guid'] = $aResult['link'];
-    $aResult['pubDate'] = date(DATE_RSS, (int)$this->getCreatedAtTimestamp());
-    if($bIsForRpc) {
+		$aResult['author'] = $this->getUserRelatedByCreatedBy()->getEmail().' ('.$this->getUserRelatedByCreatedBy()->getFullName().')';
+		$aTags = TagPeer::tagInstancesForObject($this);
+		$aCategories = array();
+		foreach($aTags as $oTag) {
+			$aCategories[] = $oTag->getTagName();
+		}
+		$aResult[$bIsForRpc ? 'categories' : 'category'] = $aCategories;
+		$aResult['guid'] = $aResult['link'];
+		$aResult['pubDate'] = date(DATE_RSS, (int)$this->getCreatedAtTimestamp());
+		if($bIsForRpc) {
 			$aResult['dateCreated'] = new xmlrpcval(iso8601_encode((int)$this->getCreatedAtTimestamp()), 'dateTime.iso8601');
 			$aResult['date_created_gmt'] = new xmlrpcval(iso8601_encode((int)$this->getCreatedAtTimestamp(), 1), 'dateTime.iso8601');
-      $aResult['postid'] = $this->getId();
-    }
-    return $aResult;
+			$aResult['postid'] = $this->getId();
+		}
+		return $aResult;
 	}
 	
 	public function getCountComments() {
@@ -84,25 +91,25 @@ class JournalEntry extends BaseJournalEntry {
 	}
 	
 	public function fillFromRssAttributes($aAttributes) {
-	  if(isset($aAttributes['categories'])) {
-      $aTags = $aAttributes['categories'];
-      $aTagInstances = TagPeer::tagInstancesForObject($this);
-      $aOldTags = array();
-      foreach($aTagInstances as $oTagInstance) {
-        if(!in_array($oTagInstance->getTagName(), $aTags)) {
-          $oTagInstance->delete();
-        } else {
-          $aOldTags[] = $oTagInstance->getTagName();
-        }
-      }
-      foreach($aTags as $sTagName) {
-        if(!in_array($sTagName, $aOldTags)) {
-          TagInstancePeer::newTagInstanceForObject($sTagName, $this);
-        }
-      }
-	  }
-    $this->setText($aAttributes['description']);
-    $this->setTitle($aAttributes['title']);
+		if(isset($aAttributes['categories'])) {
+			$aTags = $aAttributes['categories'];
+			$aTagInstances = TagPeer::tagInstancesForObject($this);
+			$aOldTags = array();
+			foreach($aTagInstances as $oTagInstance) {
+				if(!in_array($oTagInstance->getTagName(), $aTags)) {
+					$oTagInstance->delete();
+				} else {
+					$aOldTags[] = $oTagInstance->getTagName();
+				}
+			}
+			foreach($aTags as $sTagName) {
+				if(!in_array($sTagName, $aOldTags)) {
+					TagInstancePeer::newTagInstanceForObject($sTagName, $this);
+				}
+			}
+		}
+		$this->setText($aAttributes['description']);
+		$this->setTitle($aAttributes['title']);
 	}
 
 } // JournalEntry
