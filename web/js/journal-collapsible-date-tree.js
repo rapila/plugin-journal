c2f7db6cec3319befa9c0d548ce54ff01bf42f81
Jürg Messmer <jm@mosaics.ch>
2012-08-06 16:13:18 +0200

    fix display of active ol

--- web/js/journal-collapsible-date-tree.js
+++ web/js/journal-collapsible-date-tree.js
@@ -1,6 +1,6 @@
 jQuery(function() {
 	var tree = jQuery('#journal_calendar');
-	tree.find('li a').nextAll('ol').hide().end().click(function() {
+	tree.find('li a').nextAll('ol').end().click(function() {
 		return jQuery(this).nextAll('ol').toggle().length ? false : true;
 	});
 });
