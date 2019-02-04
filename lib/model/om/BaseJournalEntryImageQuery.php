61a88bec395d5aa7e6f90cd13d013b20b5eae94d
Jürg Messmer <jm@mosaics.ch>
2019-02-04 15:54:12 +0100

    fix select argument has to be an array

--- lib/model/om/BaseJournalEntryImageQuery.php
+++ lib/model/om/BaseJournalEntryImageQuery.php
@@ -981,7 +981,7 @@ abstract class BaseJournalEntryImageQuery extends ModelCriteria
     }
     public function findMostRecentUpdate($bAsTimestamp = false) {
         $oQuery = clone $this;
-        $sDate = $oQuery->clearOrderByColumns()->lastUpdatedFirst()->select("UpdatedAt")->findOne();
+        $sDate = $oQuery->clearOrderByColumns()->lastUpdatedFirst()->select(["UpdatedAt"])->findOne();
         if($sDate === null) {
             return null;
         }
