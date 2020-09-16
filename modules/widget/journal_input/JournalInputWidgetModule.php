af559bee6b479231854bb2e571c22bf68240f1b4
Jürg Messmer <jm@mosaics.ch>
2020-09-16 11:07:50 +0100

    fix alphabetical sort order

--- modules/widget/journal_input/JournalInputWidgetModule.php
+++ modules/widget/journal_input/JournalInputWidgetModule.php
@@ -3,13 +3,12 @@
  * @package modules.widget
  */
 class JournalInputWidgetModule extends WidgetModule {
-	
+
 	public function listJournals($iJournalIdsConfigured = null) {
 		$oQuery = JournalQuery::create()->distinct();
 		if($iJournalIdsConfigured !== null) {
 			$oQuery->filterById($iJournalIdsConfigured);
 		}
-		$aResult = $oQuery->orderByName()->find()->toKeyValue('Id', 'Name');
-		return $aResult;
+		return WidgetJsonFileModule::jsonBaseObjects($oQuery->orderByName()->find(), array('id', 'name'));
 	}
 }
